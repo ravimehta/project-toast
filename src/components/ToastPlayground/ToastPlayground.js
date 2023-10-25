96c10454e00032bc22a865a1a6ce83ee55361414
ravimehta <ravi@ravi-mehta.com>
2023-10-24 20:34:27 -0700

    Completed refactor to add ToastProvider

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
+import { ToastContext } from "../ToastProvider";
+import Button from "../Button";
+import ToastShelf from "../ToastShelf";
 
-import Button from '../Button';
+import styles from "./ToastPlayground.module.css";
 
-import styles from './ToastPlayground.module.css';
-
-const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
+const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
+const DEFAULT_VARIANT = VARIANT_OPTIONS[0];
 
 function ToastPlayground() {
+  const { addToast } = React.useContext(ToastContext);
+  const [message, setMessage] = React.useState("");
+  const [variant, setVariant] = React.useState(DEFAULT_VARIANT);
+
+  function handlePopToast() {
+    // Add the toast
+    addToast(variant, message);
+
+    // Reset form
+    setVariant(DEFAULT_VARIANT);
+    setMessage("");
+  }
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -14,48 +29,64 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
-      <div className={styles.controlsWrapper}>
+      <ToastShelf />
+
+      <form
+        className={styles.controlsWrapper}
+        onSubmit={(event) => {
+          event.preventDefault();
+          handlePopToast();
+        }}
+      >
         <div className={styles.row}>
           <label
             htmlFor="message"
             className={styles.label}
-            style={{ alignSelf: 'baseline' }}
+            style={{ alignSelf: "baseline" }}
           >
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              required={true}
+              id="message"
+              className={styles.messageInput}
+              value={message}
+              onChange={(event) => {
+                setMessage(event.target.value);
+              }}
+            />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            {VARIANT_OPTIONS.map((variantOption) => (
+              <label htmlFor={`variant-${variantOption}`} key={variantOption}>
+                <input
+                  id={`variant-${variantOption}`}
+                  type="radio"
+                  name="variant"
+                  value={variantOption}
+                  checked={variant === variantOption}
+                  onChange={(event) => {
+                    setVariant(event.target.value);
+                  }}
+                />
+                {variantOption}
+              </label>
+            ))}
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
             <Button>Pop Toast!</Button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import Toast from '../Toast';
-import styles from './ToastShelf.module.css';
+import Toast from "../Toast";
+import styles from "./ToastShelf.module.css";
+import { ToastContext } from "../ToastProvider";
 
 function ToastShelf() {
+  const { toasts, removeToast } = React.useContext(ToastContext);
+  function handleDismissToast(toast) {
+    removeToast(toast.id);
+  }
+
+  if (toasts.length === 0) {
+    return null;
+  }
+
   return (
     <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+      {toasts.toReversed().map((toast) => (
+        <li className={styles.toastWrapper} key={toast.id}>
+          <Toast
+            variant={toast.variant}
+            onDismiss={() => handleDismissToast(toast)}
+          >
+            {toast.message}
+          </Toast>
+        </li>
+      ))}
     </ol>
   );
 }
